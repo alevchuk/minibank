0cfdef369456a6252c611ac0fcbb6ba33c7126c4
Aleksandr Levchuk <aid_cements.0y@icloud.com>
2020-05-29 05:33:24 -0700

    Track outbound liquidity too

--- scripts/liquidity_mon.py
+++ scripts/liquidity_mon.py
@@ -17,6 +17,12 @@ from prometheus_client import Gauge, start_http_server
 
 METRICS_PORT = 6549
 
+LOCAL_BALANCE_GAUGE = Gauge(
+    'local_balance_sats',
+    'Amount of outbound liquidity (sats)',
+    ["remote_node"]
+)
+
 REMOTE_BALANCE_GAUGE = Gauge(
     'remote_balance_sats',
     'Amount of inbound liquidity (sats)',
@@ -58,6 +64,19 @@ def active_channels_with_remote_balances(channels):
   return results
 
 
+def active_channels_with_local_balances(channels):
+  results = []
+  for c in channels:
+    if c["active"] == True:
+      if int(c["local_balance"]) > 0 or True:
+        results.append(c)
+
+  if len(results) == 0:
+      print("ERROR: no channels with remote balances, so there is nothing to balance")
+
+  return results
+
+
 def main():
     logger.info("Starting metrics on port {} ...".format(METRICS_PORT))
     start_http_server(METRICS_PORT)
@@ -65,20 +84,33 @@ def main():
     
     while True:
         channels = json.loads(subprocess.check_output(["lncli", "listchannels"]).decode("utf-8"))["channels"]
+        channels_with_local = active_channels_with_local_balances(channels)
         channels_with_remote = active_channels_with_remote_balances(channels)
         
         for pubkey_prefix, local_name in PUBKEY_MAP.items():
-            total = 0
+            total_l = 0
             for c in channels_with_remote:
                 if c["remote_pubkey"].startswith(pubkey_prefix):
-                    total += int(c["remote_balance"])
+                    total_l += int(c["local_balance"])
         
-            logger.debug("{}: {}".format(local_name, total))
-            REMOTE_BALANCE_GAUGE.labels(local_name).set(total)
+            logger.debug("Local {}: {}".format(local_name, total_l))
+            LOCAL_BALANCE_GAUGE.labels(local_name).set(total_l)
+
+            total_r = 0
+            for c in channels_with_remote:
+                if c["remote_pubkey"].startswith(pubkey_prefix):
+                    total_r += int(c["remote_balance"])
         
+            logger.debug("Remote {}: {}".format(local_name, total_r))
+            REMOTE_BALANCE_GAUGE.labels(local_name).set(total_r)
+
+        total_local_balance = sum(int(c['local_balance']) for c in channels_with_local)
         total_remote_balance = sum(int(c['remote_balance']) for c in channels_with_remote)
 
-        logger.debug("total: {}".format(total_remote_balance))
+        logger.debug("total local: {}".format(total_local_balance))
+        LOCAL_BALANCE_GAUGE.labels("total").set(total_local_balance)
+
+        logger.debug("total remote: {}".format(total_remote_balance))
         REMOTE_BALANCE_GAUGE.labels("total").set(total_remote_balance)
 
         time.sleep(5)
